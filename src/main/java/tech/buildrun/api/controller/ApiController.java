541bbc725eeda40eae5019c3fe484b2509182c33
kayna <kaynanoliveira357@gmail.com>
2025-08-26 01:03:23 -0300

    Adicionando o método PUT

--- src/main/java/tech/buildrun/api/controller/ApiController.java
+++ src/main/java/tech/buildrun/api/controller/ApiController.java
@@ -11,22 +11,49 @@ import java.util.List;
 @Controller
 @RequestMapping("/users")
 public class ApiController {
-    private List<User> users = new ArrayList<>();
+    private final List<User> users = new ArrayList<>();
 
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user) {
-        users.add(user);
-        return ResponseEntity.ok(user);
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        if (user.getName().trim().isEmpty() || user.getEmail().trim().isEmpty()) {
+            return ResponseEntity.badRequest().body("Nome e email devem ser informados");
+        } else {
+            for (User u : users) {
+                if (u.getId() == user.getId()) {
+                    return ResponseEntity.badRequest().body("Id já existe");
+                }
+            }
+            users.add(user);
+            return ResponseEntity.ok("Usúario adicionado com sucesso!");
+        }
     }
 
     @GetMapping
     public ResponseEntity<List<User>> listUsers() {
-        return ResponseEntity.ok(users);
+        if (users.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.ok(users);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable int id, @RequestBody User user) {
+        if (id >= 0 && id < users.size()) {
+            users.set(id, user);
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @DeleteMapping
-    public ResponseEntity<String> deleteUsers(){
-        users.clear();
-        return ResponseEntity.ok("Usuários deletados com sucesso!");
+    public ResponseEntity<String> deleteUsers() {
+        if (users.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        } else {
+            users.clear();
+            return ResponseEntity.ok("Usuários deletados com sucesso!");
+        }
     }
 }
--- src/main/java/tech/buildrun/api/service/User.java
+++ src/main/java/tech/buildrun/api/service/User.java
@@ -3,11 +3,21 @@ package tech.buildrun.api.service;
 
 public class User {
     private String name;
+    private String email;
     private int id;
 
-    public User(String name, int id) {
+    public User(String name, int id, String email) {
         this.name = name;
         this.id = id;
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getName() {
